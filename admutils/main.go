6ebf8a7190915a3a7a52cba9ce5e2e30d3a9593d
Stanislav Dryomov <stasundr@gmail.com>
2017-03-14 02:36:19 +0700

    Hashfile

--- admutils/main.go
+++ admutils/main.go
@@ -1,14 +1,20 @@
 package admutils
 
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
 // Hashit takes a string as argument
 // and calculates hash for it
 // (see https://github.com/DReichLab/EIG/blob/master/src/admutils.c :683)
-func Hashit(str string) int {
-	var hash, length = 0, len(str)
+func Hashit(str string) int32 {
+	var hash, length int32 = 0, int32(len(str))
 
-	for j := 0; j < length; j++ {
+	for j := int32(0); j < length; j++ {
 		hash *= 23
-		hash += int(str[j])
+		hash += int32(str[j])
 	}
 
 	return hash
@@ -17,16 +23,41 @@ func Hashit(str string) int {
 // Hasharr takes an array of strings as argument
 // and calculates hash for it
 // (see https://github.com/DReichLab/EIG/blob/master/src/admutils.c :666)
-func Hasharr(xarr []string) int {
-	var hash, thash, nxarr int
-	hash = 0
-	nxarr = len(xarr)
+func Hasharr(xarr []string) int32 {
+	var hash, thash, nxarr int32
+	nxarr = int32(len(xarr))
 
-	for i := 0; i < nxarr; i++ {
+	for i := int32(0); i < nxarr; i++ {
 		thash = Hashit(xarr[i])
 		hash *= 17
 		hash ^= thash
 	}
 
 	return hash
+}
+
+// Hashfile takes a file path string as argument
+// and calculates hash for the file
+func Hashfile(path string) int32 {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var hash, thash int32
+	reader := bufio.NewReader(file)
+	for {
+		str, err := reader.ReadString(10)
+		if err != nil {
+			thash = Hashit(str)
+			hash *= 17
+			hash ^= thash
+			return hash
+		}
+
+		thash = Hashit(str[0 : len(str)-1])
+		hash *= 17
+		hash ^= thash
+	}
 }
--- main.go
+++ main.go
@@ -1,26 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"convertfs/admutils"
 	"fmt"
-	"log"
-	"os"
 )
 
 func main() {
-	file, err := os.Open("./test.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		fmt.Println(admutils.Hashit(scanner.Text()))
-	}
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println(admutils.Hashfile("test.txt"))
 }
