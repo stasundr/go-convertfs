2ba5871e6655b8f4876c5d4a8db90dd402802c1d
Stanislav Dryomov <stasundr@gmail.com>
2017-03-15 03:56:58 +0700

    packed geno to geno WIP 2

--- main.go
+++ main.go
@@ -1,54 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"convertfs/mcio"
 	"flag"
 	"fmt"
+	"io"
+	"log"
 	"math"
+	"os"
 )
 
 func main() {
+	// TODO: par file parse
 	var parFile string
 	flag.StringVar(&parFile, "p", "", "par file")
 	flag.Parse()
 	fmt.Println(parFile)
 
-	hashOk := mcio.Calcishash(
-		"/Users/me/Desktop/v19/v19.0_HO.pruned.geno",
-		"/Users/me/Desktop/v19/v19.0_HO.pruned.ind",
-		"/Users/me/Desktop/v19/v19.0_HO.pruned.snp")
+	genoPath := "/Users/me/Desktop/v19/v19.0_HO.pruned.geno"
+	indPath := "/Users/me/Desktop/v19/v19.0_HO.pruned.ind"
+	snpPath := "/Users/me/Desktop/v19/v19.0_HO.pruned.snp"
 
+	// TODO: get indNum
 	indNum := 435
 	chunkSize := int(math.Ceil(float64(indNum) / 4))
+	chunk := make([]byte, chunkSize)
+
+	file, err := os.Open(genoPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	reader := bufio.NewReaderSize(file, chunkSize)
+	// packed eigenstrat to unpacked eigenstrat core
+	// for ideas see https://github.com/stasundr/co-huge-converter/blob/master/modules/utils.js
+	for {
+		_, err := reader.Read(chunk)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+
+		for i, b := range chunk {
+			for j := uint(0); j < 4; j++ {
+				snp := (b >> (3 - j)) & 3
+				fmt.Println(i, b, snp)
+			}
+		}
+	}
+
+	hashOk := mcio.Calcishash(genoPath, indPath, snpPath)
 	fmt.Println(hashOk, chunkSize)
-
-	// https://github.com/stasundr/co-huge-converter/blob/master/modules/utils.js
-	// const indNum = meta.split(/\s+/)[1];
-	//       const chunkSize = Math.ceil(indNum/4);
-
-	//       // 11000000 - 192
-	//       // 00110000 - 48
-	//       // 00001100 - 12
-	//       // 00000011 - 3
-	//       // const mask = [192, 48, 12, 3];
-
-	//       let stream = fs.createReadStream(path, { encoding: null, start: chunkSize });
-	//       stream.on('error', reject);
-	//       stream.on('readable', () => {
-	//         let chunk;
-	//         while ((chunk = stream.read(chunkSize)) !== null) {
-	//           //let snp = (chunk.readUInt8(Math.ceil(index/4)) << ((index % 4) * 2)) & 192;
-	//           let snp = (chunk[Math.floor(index/4)] >> ((3 - index % 4) * 2)) & 3;
-
-	//           byte = byte | (snp << (6 - bitpair * 2));
-	//           bitpair++;
-
-	//           if (bitpair == 4) {
-	//             geno.push(byte);
-	//             byte = 0;
-	//             bitpair = 0;
-	//           }
-	//         }
-	//       });
-	//       stream.on('close', () => resolve(geno));
 }
